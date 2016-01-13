c4f7ea12c3a3ca121392e0769466bc67ca5ac68f
wuyuanyi135 <wuyuanyi135@gmail.com>
2016-01-13 01:48:45 -0500

    drawer button added

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,31 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
 var connect = require('gulp-connect');
-gulp.task('build', function () {
-	return gulp.src('src/index.html')
-		.pipe(vulcanize({
-			abspath: '',
-			excludes: [],
-			inlineScripts: true,
-			stripExcludes: false
-		}))
-		.pipe(gulp.dest('dest'))
-		.pipe(connect.reload());
+gulp.task('build', function() {
+  var stream = gulp.src('src/index.html')
+    .pipe(vulcanize({
+      abspath: '',
+      excludes: [],
+      inlineScripts: true,
+      stripExcludes: false
+    }))
+		.on('error',function(error){
+			console.log(error.toString());
+			this.emit('end');
+		})
+    .pipe(gulp.dest('dest'))
+    .pipe(connect.reload());
+
+
+  return stream;
 });
 
 gulp.task('default', function() {
-	connect.server({
-		root: "dest",
-		livereload: true
-	})
-	gulp.watch(["src/**"],['build']);
+  connect.server({
+    root: "dest",
+    livereload: true
+  })
+
+  gulp.watch(["src/**"], ['build']);
 
 })
